30af8ee2230a05987bc04acfe947f3afc0509981
Elizaveta Belkina <elizaveta.belkina@berkeley.edu>
2022-11-16 04:05:26 +0000

    created new migration with new attributes

---
+++ db/migrate/20221116040052_add_more_columns_to_representatives.rb
@@ -0,0 +1,10 @@
+class AddMoreColumnsToRepresentatives < ActiveRecord::Migration[5.2]
+  def change
+    add_column :representatives, :street, :string
+    add_column :representatives, :city, :string
+    add_column :representatives, :state, :string
+    add_column :representatives, :zip, :string
+    add_column :representatives, :party, :string
+    add_column :representatives, :photo_url, :string
+  end
+end
