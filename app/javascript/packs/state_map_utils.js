20efb3b9e591bbf4d328761994f11b06f369f294
Nathaniel Haynam <natehaynam@berkeley.edu>
2022-11-17 09:51:02 +0000

    county representative view update

--- app/javascript/packs/state_map_utils.js
+++ app/javascript/packs/state_map_utils.js
@@ -65,6 +65,8 @@ exports.setupEventHandlers = (stateMap) => {
     const clickCallback = (elem) => {
         const countyFipsCode = elem.attr('data-county-fips-code');
         window.location.href = `/state/${stateMap.state.symbol}/county/${countyFipsCode}`;
+        const countyName = elem.attr('data-county-name');
+        window.location.href = `/search/${countyName}`;
     };
     mapUtils.handleMapMouseEvents(targets, hoverHtmlProvider, clickCallback);
 };
